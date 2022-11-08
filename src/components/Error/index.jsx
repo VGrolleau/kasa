d0233ed57c127a48b87ce149f53f12939ee4f0b6
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-11-08 17:14:13 +0100

    Finish error page style

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react"
+import Header from '../../components/Header/index.jsx';
+import Footer from '../../components/Footer';
+import { Link } from "react-router-dom";
+import '../../utils/styles/Error.css'
+
 function Error() {
+    useEffect(() => { document.title = "Kasa - 404" })
     return (
         <div>
-            <h1>Oups...</h1>
-            <img src="https://github.com/atoulmet/assets/blob/master/404.svg?raw=true" alt="" />
-            <p>
-                Il semblerait que la page que vous cherchez n’existe pas
-            </p>
+            <Header />
+            <section className="section-error">
+                <h1>404</h1>
+                <p>
+                    Oups! La page que vous demandez n’existe pas.
+                </p>
+                <Link to='/'>Retourner à la page d'accueil</Link>
+            </section>
+            <Footer />
         </div>
     )
 }
