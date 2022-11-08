2ce82798e5fb843123d4bdf11c630631c2c1ac5d
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-11-08 16:23:34 +0100

    Try fixing about dropdown problem

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,31 +1,69 @@
 import '../../utils/styles/Collapse.css'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Collapse(props) {
     const [isOpen, setIsOpen] = useState(false)
 
     const collapseContents = props.collapseContents;
-    const collapseDivOpen = collapseContents.map((collapseContent) =>
-        <div className="collapse">
-            <button className='collapse-button' key={collapseContent.id} onClick={(e) => {
-                console.log(e.target)
-                setIsOpen(false)
+    // let clickedElementId = e.target.parentElement.id;
+    const clickedElementClass = "open";
+
+    // collapseContents.forEach(collapseContent => {
+    //     if (collapseContent.id === clickedElementId) console.log(e.target.parentElement)
+    // })
+
+    // return (
+    //     collapseContents.map((collapseContent, index) =>
+    //         <div className="collapse" key={`${collapseContent.text}-${index}`} id={index}>
+    //             <div className='collapse-button'>
+    //                 {collapseContent.text} <span className="fa-solid fa-angle-down select-arrow"></span>
+    //             </div>
+    //             <div className="collapse-content" style={{ display: "block" }}>{collapseContent.content}</div>
+    //         </div>
+    //     )
+    // )
+    // console.log(clickedElementId, props.clickedElementId);
+
+    return isOpen ? (
+        collapseContents.map((collapseContent, index) =>
+            <div className="collapse open" key={`${collapseContent.text}-${index}`} id={collapseContent.id} onClick={(e) => {
+                // console.log(e.target)
+                // setIsOpen(false)
+                // console.log(isOpen, setIsOpen)
+                // console.log(collapseContent.id, e.target.parentElement.id, e.target.parentElement)
+                if (collapseContent.id === parseInt(e.target.parentElement.id)) {
+                    console.log(e.target.parentElement)
+                    setIsOpen(false)
+                    e.target.parentElement.classList.add("open")
+                }
             }}>
-                {collapseContent.text} <span className="fa-solid fa-angle-down select-arrow"></span>
-            </button>
-            <p className="collapse-content" style={{ display: "block" }}>{collapseContent.content}</p>
-        </div>
-    )
-    const collapseDivClose = collapseContents.map((collapseContent) =>
-        <div className="collapse">
-            <button className='collapse-button' key={collapseContent.id} onClick={() => setIsOpen(true)}>
-                {collapseContent.text} <span className="fa-solid fa-angle-down select-arrow"></span>
-            </button>
-            <p className="collapse-content" style={{ display: "none" }}>{collapseContent.content}</p>
-        </div>
+                <div className='collapse-button'>
+                    {collapseContent.text} <span className="fa-solid fa-angle-down select-arrow"></span>
+                </div>
+                <div className="collapse-content" style={{ display: "block" }}>{collapseContent.content}</div>
+            </div>
+        )
+    ) : (
+        collapseContents.map((collapseContent, index) =>
+            <div className="collapse" key={`${collapseContent.text}-${index}`} id={collapseContent.id} onClick={(e) => {
+                console.log(collapseContent.id === parseInt(e.target.parentElement.id))
+                // setIsOpen(true)
+                // console.log(clickedElementId)
+                // console.log(isOpen, setIsOpen)
+                if (collapseContent.id === parseInt(e.target.parentElement.id)) {
+                    console.log(e.target.parentElement)
+                    setIsOpen(true)
+                    e.target.parentElement.classList.remove("open")
+                }
+            }}>
+                <div className='collapse-button'>
+                    {collapseContent.text} <span className="fa-solid fa-angle-down select-arrow"></span>
+                </div>
+                <div className="collapse-content" style={{ display: "none" }}>{collapseContent.content}</div>
+            </div>
+        )
     )
 
-    return isOpen ? collapseDivOpen : collapseDivClose
     // return collapseDivOpen
 }
 
--- src/components/Collapses/index.jsx
+++ src/components/Collapses/index.jsx
@@ -4,30 +4,41 @@ import '../../utils/styles/Collapses.css'
 function Collapses() {
     const collapseContents = [
         {
-            "id": "1",
+            "id": 1,
             "text": "Fiabilité",
             "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem."
         },
         {
-            "id": "2",
+            "id": 2,
             "text": "Respect",
             "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
         },
         {
-            "id": "3",
+            "id": 3,
             "text": "Service",
             "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem."
         },
         {
-            "id": "4",
+            "id": 4,
             "text": "Responsabilité",
             "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem."
         },
     ]
 
+    let clickedElementId;
+    const clickedElementClass = "open";
+
     return (
-        <section className="collapse-section">
-            <Collapse collapseContents={collapseContents} />
+        <section className="collapse-section" onClick={(e) => {
+            console.log(document.querySelectorAll(".collapse"), e.target.parentElement.id, e.target.textContent)
+            // document.querySelectorAll(".collapse").foreach((collapse) => {
+            //     console.log(collapse, e.target.parentElement.id)
+            // })
+            // clickedElementId = e.target.parentElement.id;
+            // // e.target.parentElement.classList.add(clickedElementClass);
+            // console.log(clickedElementId);
+        }}>
+            <Collapse collapseContents={collapseContents} clickedElementId={clickedElementId} clickedElementClass={clickedElementClass} />
         </section>
     )
 }
